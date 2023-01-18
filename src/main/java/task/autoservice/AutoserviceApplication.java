4dc4774f633179f22457df12a17f7a4f5de38ea5
thebitler <yurchyknikita@gmail.com>
2023-01-18 19:33:06 +0100

    Spring boot initialization

---
+++ src/main/java/task/autoservice/AutoserviceApplication.java
@@ -0,0 +1,13 @@
+package task.autoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoserviceApplication.class, args);
+	}
+
+}
