9ee31b5d287b49c815b4a573f110cd3b1107d35c
thebitler <yurchyknikita@gmail.com>
2023-01-23 23:09:44 +0100

    Update CarOwnerController. Add get user's orders (completed)

--- src/main/java/task/autoservice/controller/CarOwnerController.java
+++ src/main/java/task/autoservice/controller/CarOwnerController.java
@@ -1,5 +1,6 @@
 package task.autoservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -7,20 +8,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import task.autoservice.dto.mapper.CarOwnerMapper;
+import task.autoservice.dto.mapper.OrderMapper;
 import task.autoservice.dto.request.CarOwnerRequestDto;
 import task.autoservice.dto.response.CarOwnerResponseDto;
+import task.autoservice.dto.response.OrderResponseDto;
 import task.autoservice.model.CarOwner;
 import task.autoservice.service.CarOwnerService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/car-owners")
 public class CarOwnerController {
     private final CarOwnerService carOwnerService;
     private final CarOwnerMapper carOwnerMapper;
+    private final OrderMapper orderMapper;
 
-    public CarOwnerController(CarOwnerService carOwnerService, CarOwnerMapper carOwnerMapper) {
+    public CarOwnerController(
+            CarOwnerService carOwnerService,
+            CarOwnerMapper carOwnerMapper,
+            OrderMapper orderMapper
+    ) {
         this.carOwnerService = carOwnerService;
         this.carOwnerMapper = carOwnerMapper;
+        this.orderMapper = orderMapper;
     }
 
     @PostMapping
@@ -35,6 +46,12 @@ public class CarOwnerController {
             @RequestBody CarOwnerRequestDto requestDto) {
         CarOwner updatedCarOwner = carOwnerService.update(carOwnerMapper.toModel(id, requestDto));
         return carOwnerMapper.toDto(updatedCarOwner);
+    }
+
+    @GetMapping("/{id}/orders")
+    public List<OrderResponseDto> getOrders(@PathVariable Long id) {
+        return carOwnerService.getById(id).getOrders()
+                .stream().map(orderMapper::toDto).toList();
     }
 }
 
