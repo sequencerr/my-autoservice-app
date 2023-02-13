578ed537b0763d3bdb5468965e952431fccf8ce3
thebitler <yurchyknikita@gmail.com>
2023-02-13 19:38:43 +0100

    Fix small after renaming entities missed changes + toString() + OverhaulRepository aliases

--- src/main/java/task/autoservice/repository/OverhaulRepository.java
+++ src/main/java/task/autoservice/repository/OverhaulRepository.java
@@ -11,18 +11,18 @@ import java.util.List;
 
 @Repository
 public interface OverhaulRepository extends JpaRepository<Overhaul, Long> {
-    @Query(value = "SELECT cs.id id, cs.price price, cs.is_paid is_paid,"
-            + " cs.order_id order_id, cs.repairer_id repairer_id"
+    @Query(value = "SELECT os.id id, os.price price, os.is_paid is_paid,"
+            + " os.order_id order_id, os.repairer_id repairer_id"
             + " FROM orders o"
-            + " JOIN orders_overhauls ocs ON ocs.order_id = o.id"
-            + " JOIN overhauls        cs  ON cs.order_id  = ocs.overhauls_id "
-            + " JOIN repairers           r   ON r.id         = cs.repairer_id"
+            + " JOIN orders_overhauls oos ON oos.order_id = o.id"
+            + " JOIN overhauls        os  ON os.order_id  = oos.overhauls_id "
+            + " JOIN repairers           r   ON r.id         = os.repairer_id"
             + " WHERE o.status = ?2 AND r.id = ?1", nativeQuery = true)
     List<Overhaul> getOverhaulsByRepairerAndOrderStatus(Long repairerId, OrderStatus status);
 
     @Modifying
-    @Query(value = "UPDATE overhauls s"
-            + " SET s.is_paid = TRUE"
-            + " WHERE s.id IN :ids AND s.is_paid = FALSE", nativeQuery = true)
+    @Query(value = "UPDATE overhauls os"
+            + " SET os.is_paid = TRUE"
+            + " WHERE os.id IN :ids AND os.is_paid = FALSE", nativeQuery = true)
     void markAllPaidById(List<Long> ids);
 }
