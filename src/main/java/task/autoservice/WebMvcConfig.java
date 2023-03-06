3768fddd2af06b8b302c6349ca23a3261a7a1029
thebitler <yurchyknikita@gmail.com>
2023-03-06 18:28:31 +0100

    Update API url path to context/api/...

---
+++ src/main/java/task/autoservice/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package task.autoservice;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        HandlerTypePredicate predicate = HandlerTypePredicate.forAnnotation(RestController.class);
+        configurer.addPathPrefix("api", predicate);
+    }
+}
